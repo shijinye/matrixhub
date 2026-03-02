17f22d7c6a185b569e19277862ca0adc28bf48b1
setsunakute <fripsidenice@gmail.com>
2026-03-02 15:23:36 +0800

    add login proto

---
+++ api/ts/v1alpha1/login.pb.ts
@@ -0,0 +1,29 @@
+/* eslint-disable */
+// @ts-nocheck
+/*
+* This file is a generated Typescript file for GRPC Gateway, DO NOT MODIFY
+*/
+
+import * as fm from "../fetch.pb"
+export type LoginRequest = {
+  username?: string
+  password?: string
+}
+
+export type LoginResponse = {
+}
+
+export type LogoutRequest = {
+}
+
+export type LogoutResponse = {
+}
+
+export class Login {
+  static Login(req: LoginRequest, initReq?: fm.InitReq): Promise<LoginResponse> {
+    return fm.fetchReq<LoginRequest, LoginResponse>(`/api/v1alpha1/login`, {...initReq, method: "POST"})
+  }
+  static Logout(req: LogoutRequest, initReq?: fm.InitReq): Promise<LogoutResponse> {
+    return fm.fetchReq<LogoutRequest, LogoutResponse>(`/api/v1alpha1/logout`, {...initReq, method: "POST", body: JSON.stringify(req, fm.replacer)})
+  }
+}
